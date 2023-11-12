1b5dbce556b9c1a296fc8357eb42264fad6a2ba6
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-11-12 02:07:42 -0500

    updated MongoDB connection string

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
-mongoose.connect('', {
+mongoose.connect('mongodb://localhost:27017', {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true,
